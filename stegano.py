06622c944ad58b16f308b251baff12c9e8490ce9
Julie LAM VAN BA <92156620+julielvb@users.noreply.github.com>
2021-12-02 12:05:24 +0100

    Add files via upload

---
+++ stegano.py
@@ -0,0 +1,11 @@
+# installer stegano
+
+from stegano import lsb
+
+secret = lsb.hide("kaiju.png", "Hello mama")
+secret.save("kaiju_secret.png")
+clear_message = lsb.reveal("kaiju_secret.png")
+
+# sur linux
+# stegano-lsb hide -i [name].png -m "Secret Message" -o [newname].png
+# stegano-lsb reveal -i [name].png
