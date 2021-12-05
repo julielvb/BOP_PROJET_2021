37731bf2ecec0e5db6fb5ec8c1553fc4347b28bc
Julie LAM VAN BA <92156620+julielvb@users.noreply.github.com>
2021-12-06 00:25:30 +0100

    Update stegano_JULIE.py

--- stegano_JULIE.py
+++ stegano_JULIE.py
@@ -5,6 +5,7 @@ from tkinter import font as tkFont      # module des polices d'écriture
 from tkinter.filedialog import *        # module de récupération d'une image
 # importation des fonctionnalités de PIL
 from PIL import ImageTk, Image
+import piexif
 # importation des fonctionnalités de STEGANO
 from stegano import lsb
 from stegano import exifHeader as aaa
@@ -34,7 +35,7 @@ def encode():
         fileopen = askopenfilename(initialdir="/Desktop", title="sélectionner un fichier",
                                    filetypes=(("jpeg,png files", "*jpg *png"), ("all files", "*.*")))
         imagee = ImageTk.PhotoImage(Image.open(fileopen))
-        Labelpath = Label(text=fileopen)
+        Labelpath = Label(text=fileopen, bg='ivory')
         Labelpath.place(relx=0.3, rely=0.25, height=21, width=450)
         Labelimg = Label(image=imagee)
         Labelimg.place(relx=0.3, rely=0.3, height=200, width=200)
@@ -42,23 +43,23 @@ def encode():
     Button2 = Button(text="Ouvrir un fichier", command=openfile)
     Button2.place(relx=0.3, rely=0.2, height=31, width=94)
 
+    Label1 = Label(text="Entrer un message")
+    Label1.place(relx=0.3, rely=0.55, height=21)
+    entrysecmes = Entry()
+    entrysecmes.place(relx=0.45, rely=0.55, relheight=0.05, relwidth=0.200)
+
+    Label2 = Label(text="Nouveau nom du fichier")
+    Label2.place(relx=0.3, rely=0.65, height=21, width=134)
+    entrysave = Entry()
+    entrysave.place(relx=0.45, rely=0.65, relheight=0.05, relwidth=0.200)
+
     # BOUTON DE SELECTION ENTRE JPEG (RADIO1) ET PNG (RADIO2) POUR CONVERTIR LE FICHIER ENCODE
     secimg = StringVar()
     radio1 = Radiobutton(text='jpeg', value='jpeg', variable=secimg)
-    radio1.place(relx=0.3, rely=0.57)
+    radio1.place(relx=0.3, rely=0.73)
 
     radio2 = Radiobutton(text='png', value='png', variable=secimg)
-    radio2.place(relx=0.5, rely=0.57)
-
-    Label1 = Label(text="Entrer un message")
-    Label1.place(relx=0.3, rely=0.6, height=21)
-    entrysecmes = Entry()
-    entrysecmes.place(relx=0.45, rely=0.6, relheight=0.05, relwidth=0.200)
-
-    Label2 = Label(text="Nouveau nom du fichier")
-    Label2.place(relx=0.3, rely=0.70, height=21, width=134)
-    entrysave = Entry()
-    entrysave.place(relx=0.45, rely=0.70, relheight=0.05, relwidth=0.200)
+    radio2.place(relx=0.5, rely=0.73)
 
     # MESSAGE POPUP POUR L'ENCODAGE
     def encode():
@@ -95,7 +96,7 @@ def encode():
 
     enc.mainloop()
 
-# FONCTION PUR DECODER
+# FONCTION POUR DECODER
 def decode():
     main.destroy()
     dec = Tk()
@@ -110,13 +111,6 @@ def decode():
     LabelTitle['font'] = fontl
     LabelTitle.place(relx=0.3, rely=0.1)
 
-    secimg = StringVar()
-    radio1 = Radiobutton(text='jpeg', value='jpeg', variable=secimg)
-    radio1.place(relx=0.3, rely=0.57)
-
-    radio2 = Radiobutton(text='png', value='png', variable=secimg)
-    radio2.place(relx=0.4, rely=0.57)
-
     def openfile():
         global fileopen
         global imagee
@@ -125,26 +119,21 @@ def decode():
                                    filetypes=(("jpeg files, png file", "*jpg *png"), ("all files", "*.*")))
 
         imagee = ImageTk.PhotoImage(Image.open(fileopen))
-        Labelpath = Label(text=fileopen)
+        Labelpath = Label(text=fileopen, bg='ivory')
         Labelpath.place(relx=0.3, rely=0.25, height=21, width=450)
         Labelimg = Label(image=imagee)
         Labelimg.place(relx=0.3, rely=0.3, height=200, width=200)
 
     def deimg():
-        if secimg.get() == "png":
-            messag = lsb.reveal(fileopen)
-
-        if secimg.get() == "jpeg":
-            messag = aaa.reveal(fileopen)
-
-        Label2 = Label(text=messag)
-        Label2.place(relx=0.3, rely=0.7, height=21, width=204)
+        messag = lsb.reveal(fileopen)
+        Label2 = Label(text=messag, bg='ivory')
+        Label2.place(relx=0.3, rely=0.55, height=21, width=204)
 
     Button2 = Button(text="Ouvrir un fichier", command=openfile)
     Button2.place(relx=0.3, rely=0.2, height=31, width=94)
 
     Button2 = Button(text="DECODER", command=deimg)
-    Button2.place(relx=0.3, rely=0.8, height=31, width=94)
+    Button2.place(relx=0.3, rely=0.7, height=31, width=94)
 
     def back():
         dec.destroy()
@@ -153,10 +142,9 @@ def decode():
         Popen('python steganography.py')
 
     Buttonback = Button(text="QUITTER", command=back)
-    Buttonback.place(relx=0.3, rely=0.85, height=31, width=94)
+    Buttonback.place(relx=0.3, rely=0.75, height=31, width=94)
 
     dec.mainloop()
-
 
 # PROGRAMME PRINCIPAL
 main = Tk()
@@ -170,16 +158,18 @@ label.pack()
 
 encbutton = Button(text='CACHER UN MESSAGE', fg="white", bg="black", width=25, command=encode)
 encbutton['font'] = fontl
-encbutton.place(relx=0.3, rely=0.2)
+encbutton.place(relx=0.3, rely=0.1)
 
 decbutton = Button(text='REVELER UN MESSAGE', fg="white", bg="black", width=25, command=decode)
 decbutton['font'] = fontl
-decbutton.place(relx=0.3, rely=0.4)
+decbutton.place(relx=0.3, rely=0.25)
 
+metabutton = Button(text='AFFICHER METADONNEES', fg="white", bg="black", width=25)
+metabutton['font'] = fontl
+metabutton.place(relx=0.3, rely=0.4)
 
 def exit():
     main.destroy()
-
 
 closebutton = Button(text='QUITTER', fg="white", bg="red", width=25, command=exit)
 closebutton['font'] = fontl
