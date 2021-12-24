c91323854e1ed83056d44644cd07b845e285881c
Julie LAM VAN BA <92156620+julielvb@users.noreply.github.com>
2021-12-24 19:11:25 +0100

    Update stegano.py

--- stegano.py
+++ stegano.py
@@ -1,9 +1,21 @@
 from stegano import lsb
+from stegano import exifHeader
 
-secret = lsb.hide("kaiju.png", "Hello mama")
-secret.save("kaiju_secret.png")
-clear_message = lsb.reveal("kaiju_secret.png")
-print("Message caché avec succès.\n",clear_message)
+filename = input("Nom ou chemin du fichier : ")
+secmes = input("Message secret : ")
+format = input("Format du fichier : ")
+newname = input("Nouveau nom du fichier : ")
+
+if format == "png":
+    secret = lsb.hide(filename, secmes)
+    secret.save(newname)
+    clear_message = lsb.reveal(newname)
+    print("Message caché avec succès.\n",clear_message)
+
+if format == "jpg":
+    secret = exifHeader.hide(filename, newname, secret_message=secmes)
+    print("Message caché avec succès.\n",exifHeader.reveal(newname))
+
 
 # mode terminal
 # stegano-lsb hide -i [name].png -m "Secret Message" -o [newname].png
