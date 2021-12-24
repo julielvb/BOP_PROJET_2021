4e4a6707470c4334205bf36db90b671240df3193
Julie LAM VAN BA <92156620+julielvb@users.noreply.github.com>
2021-12-24 18:09:47 +0100

    Update stegano.py

--- stegano.py
+++ stegano.py
@@ -3,8 +3,7 @@ from stegano import lsb
 secret = lsb.hide("kaiju.png", "Hello mama")
 secret.save("kaiju_secret.png")
 clear_message = lsb.reveal("kaiju_secret.png")
-print("Message caché avec succès.")
-print(clear_message)
+print("Message caché avec succès.\n",clear_message)
 
 # mode terminal
 # stegano-lsb hide -i [name].png -m "Secret Message" -o [newname].png
